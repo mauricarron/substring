ccc20c2faa41a01ee97e908c2da54c4ad380ef5c
mdcarron <mdcarron.dev@gmail.com>
2021-02-04 10:05:26 -0300

    Add user input

--- script.rb
+++ script.rb
@@ -16,6 +16,9 @@ end
 
 text = "Implement a method #substrings that takes a word as the first argument and then an array of valid substrings (your dictionary) as the second argument. It should return a hash listing each substring (case insensitive) that was found in the original string and how many times it was found."
 
+print "Write a text to analyze how many times each word matches with our dictionay: "
+user_input = gets.chomp
+
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-puts substrings(text, dictionary)
+puts substrings(user_input, dictionary)
