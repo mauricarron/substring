c447d4a88bdb6ebd686a9af55a4ad12ba98cad06
mdcarron <mdcarron.dev@gmail.com>
2021-02-04 10:02:02 -0300

    Add main logic

--- script.rb
+++ script.rb
@@ -1,1 +1,21 @@
-puts "Hello World"
+def substrings(text, dictionary)
+    text_lowercase = text.downcase
+    text_arr = text_lowercase.split(" ")
+    
+    substring = Hash.new(0)
+    text_arr.each do |word|
+        dictionary.each do |dict|
+            if word == dict
+                substring[word] += 1
+            end
+        end
+    end
+
+    return substring
+end
+
+text = "Implement a method #substrings that takes a word as the first argument and then an array of valid substrings (your dictionary) as the second argument. It should return a hash listing each substring (case insensitive) that was found in the original string and how many times it was found."
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+puts substrings(text, dictionary)
